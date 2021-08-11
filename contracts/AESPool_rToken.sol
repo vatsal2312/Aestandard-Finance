97e2e5a13e5d65e36cce2813575d4313c1bd7af0
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-08-11 14:44:42 +0530

    Update AESPool_rToken.sol

--- contracts/AESPool_rToken.sol
+++ contracts/AESPool_rToken.sol
@@ -76,7 +76,7 @@ contract AESPoolrToken is ReentrancyGuard {
         return ((number * percent) / 10000);
     }
 
-    function TotalStakingBalance() public view nonReentrant returns (uint result) {
+    function TotalStakingBalance() public nonReentrant returns (uint result) {
       uint stakingTotal = 0;
       for (uint x = 0; x < stakers.length; x++){
         stakingTotal = stakingTotal + stakingBalance[stakers[x]];
